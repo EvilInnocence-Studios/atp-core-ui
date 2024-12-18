f31e18b6ac664623068e63f84b2e7455caade559
Andrew Wittrock <andy@wittrock.us>
2024-12-18 15:42:55 -0600

    Add common services

--- lib/services.ts
+++ lib/services.ts
@@ -1,6 +1,8 @@
+import { commonServices } from "@common/lib/services";
 import { uacServices } from "@uac/lib/services";
 import { IMethods } from "./types.d";
 
 export const apiServices = (methods:IMethods) => ({
+    ...commonServices(methods),
     ...uacServices(methods),
 });
