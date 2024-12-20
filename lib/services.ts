2cb21909cff1d9a904131ecb8cfdc3bb6cad1f5e
Andrew Wittrock <andy@wittrock.us>
2024-12-20 00:28:49 -0600

    Add store services

--- lib/services.ts
+++ lib/services.ts
@@ -1,8 +1,11 @@
 import { commonServices } from "@common/lib/services";
 import { uacServices } from "@uac/lib/services";
 import { IMethods } from "./types.d";
+import { storeServices } from "@store/services";
 
+// TODO: Move this into main app config
 export const apiServices = (methods:IMethods) => ({
     ...commonServices(methods),
     ...uacServices(methods),
+    ...storeServices(methods),
 });
