a0fa0e61ae7fd3726b6fb91dcdf868864c7908a8
Andrew Wittrock <andy@wittrock.us>
2024-12-21 15:30:00 -0600

    Upgrade useLoader to handle multiple simultaneous calls

--- lib/useLoader.ts
+++ lib/useLoader.ts
@@ -2,9 +2,23 @@ import { useState } from "react";
 
 export const useLoader = () => {
     const [isLoading, setIsLoading] = useState(false);
+    const [, setInvocationCount] = useState(0);
 
-    const start = () => setIsLoading(true);
-    const stop = () => setIsLoading(false);
+    const start = () => {
+        setInvocationCount(prevCount => prevCount + 1);
+        setIsLoading(true);
+    };
+
+    const stop = () => {
+        setInvocationCount(prevCount => {
+            const newCount = Math.max(prevCount - 1, 0);
+            if (newCount === 0) {
+                setIsLoading(false);
+                return 0;
+            }
+            return newCount;
+        });
+    };
 
     return {isLoading, start, stop};
 }
