0c7ca1968776c0c3a9501bada95d1fa0f006ee99
Andrew Wittrock <andy@wittrock.us>
2025-01-09 08:51:22 -0600

    Add page size options to paginator

--- lib/usePaginator.ts
+++ lib/usePaginator.ts
@@ -3,20 +3,22 @@ import { useState } from "react";
 export declare interface IPaginator {
     current: number;
     pageSize: number;
+    defaultPageSize: number;
+    pageSizeOptions: number[];
     onChange: (page: number, pageSize: number) => void;
     showTotal: (total: number, range: [number, number]) => string;
 }
 
 export const usePaginator = ():IPaginator => {
     const [current, setCurrent] = useState(1);
-    const [pageSize, setPageSize] = useState(10);
+    const [pageSize, setPageSize] = useState(12);
 
     return {
         current,
         pageSize,
+        defaultPageSize: 12,
+        pageSizeOptions: [12, 24, 48, 96],
         onChange: (page: number, pageSize: number) => {
-            console.log('page', page);
-            console.log('pageSize', pageSize);
             setCurrent(page);
             setPageSize(pageSize);
         },
