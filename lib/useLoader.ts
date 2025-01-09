e303d92eeb6c11e042830a30e5558854787f93f2
Andrew Wittrock <andy@wittrock.us>
2025-01-09 09:36:58 -0600

    Add async loader

--- lib/useLoader.ts
+++ lib/useLoader.ts
@@ -22,3 +22,43 @@ export const useLoader = () => {
 
     return {isLoading, start, stop};
 }
+
+// LoaderAsync is a type that is a function that also has a single property isLoading of type boolean
+export type LoaderAsync = {
+    (f: () => Promise<any>): void;
+    isLoading: boolean;
+}
+
+export const useLoaderAsync = ():LoaderAsync => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [, setInvocationCount] = useState(0);
+
+    const start = () => {
+        setInvocationCount(prevCount => prevCount + 1);
+        setIsLoading(true);
+    };
+
+    const stop = () => {
+        setInvocationCount(prevCount => {
+            const newCount = Math.max(prevCount - 1, 0);
+            if (newCount === 0) {
+                setIsLoading(false);
+                return 0;
+            }
+            return newCount;
+        });
+    };
+
+    const loader = async (f: () => Promise<any>) => {
+        start();
+        try {
+            await f();
+        } finally {
+            stop();
+        }
+    };
+
+    loader.isLoading = isLoading;
+
+    return loader;
+}
