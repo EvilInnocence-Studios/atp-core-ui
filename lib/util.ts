d78bc1aa4dd001466cd45cf49739fa4545499b12
Andrew Wittrock <andy@wittrock.us>
2025-01-09 14:12:09 -0600

    Add more error handling code

--- lib/util.ts
+++ lib/util.ts
@@ -1,7 +1,12 @@
 import request from "superagent";
+import { pipe, prop } from "ts-functional";
 import { Setter } from "unstateless";
+import { flash } from "./flash";
 
 export const getResults = <T>(response:request.Response):T => response.body as T;
+export const getError = (e:any) => e.response?.body || e;
+export const execute = <T>(fn:() => T) => fn();
+export const handleError = pipe(getError, prop("message"), flash.error, execute)
 
 export const clear = (...setters:Setter<string>[]) => () => setters.forEach(setter => setter(''));
 
