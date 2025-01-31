df714e64593cd5fa4916c86fa16fbd55c753785e
Andrew Wittrock <andy@wittrock.us>
2025-01-31 13:20:58 -0600

    Update scroll to top component

--- components/ScrollToTop/ScrollToTop.component.tsx
+++ components/ScrollToTop/ScrollToTop.component.tsx
@@ -1,7 +1,7 @@
+import { faCircleUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {ScrollToTopProps} from "./ScrollToTop.d";
+import { ScrollToTopProps } from "./ScrollToTop.d";
 import styles from './ScrollToTop.module.scss';
-import { faCircleUp, faScroll } from "@fortawesome/free-solid-svg-icons";
 
 export const ScrollToTopComponent = ({toTop}:ScrollToTopProps) =>
     <FontAwesomeIcon icon={faCircleUp} className={styles.scrollToTop} onClick={toTop} />;
