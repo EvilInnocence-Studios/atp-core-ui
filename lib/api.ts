577a6d9aedfe31ea0cbe212ef29c81e834593beb
Andrew Wittrock <andy@wittrock.us>
2025-02-03 10:06:23 -0600

    Memoize service creation

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,8 @@
 import { config } from "@config";
 import { apiMethods } from "./methods";
 import { apiServices } from "./services";
+import { memoize } from "lodash";
 
 export const api = () => apiMethods(config());
 
-export const services = () => apiServices(api());
+export const services = memoize(() => apiServices(api()));
