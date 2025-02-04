caab11f5014e3964e5157b73b91d28f00370b35b
Andrew Wittrock <andy@wittrock.us>
2025-02-03 22:25:57 -0600

    Add useToggleList hook

--- lib/useToggle.ts
+++ lib/useToggle.ts
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Index } from "ts-functional/dist/types";
 
 export declare interface IToggle {
     isset:boolean;
@@ -14,4 +15,23 @@ export const useToggle = (initialValue:boolean = false):IToggle => {
     const off = () => setIsset(false);
 
     return { isset, toggle, on, off };
+}
+
+export declare interface IGroupToggle {
+    isset: (groupId:string) => boolean;
+    toggle: (groupId:string) => void;
+    on: (groupId:string) => void;
+    off: (groupId:string) => void;
+}
+
+export const useToggleList = (initialValues:Index<boolean>):IGroupToggle => {
+    const [isset, setIsset] = useState(initialValues);
+    const toggle = (groupId:string) => setIsset({...isset, [groupId]: !isset[groupId]});
+    const on = (groupId:string) => setIsset({...isset, [groupId]: true});
+    const off = (groupId:string) => setIsset({...isset, [groupId]: false});
+
+    return {
+        isset: (groupId:string) => isset[groupId],
+        toggle, on, off
+    };
 }
