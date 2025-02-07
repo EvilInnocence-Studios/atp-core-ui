c1af71ed626e7ed24a26bc32313c3356490d9d94
Andrew Wittrock <andy@wittrock.us>
2025-02-07 15:23:02 -0600

    Remove google analytics

--- lib/usePageTracking.ts
+++ lib/usePageTracking.ts
@@ -1,17 +1,7 @@
 import { config } from "@config";
 import { useEffect } from "react";
-import ga from 'react-ga4';
 import { useLocation } from "react-router-dom";
 import request from 'superagent';
-
-ga.initialize(
-    config().analytics.google,
-    {
-        gtagOptions: {
-            send_page_view: false,
-        },
-    }
-);
 
 const usePageTracking = () => {
   const location = useLocation();
