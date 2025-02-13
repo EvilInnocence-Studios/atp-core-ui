408a279ddb83d9a1dbd547e95457d6dd7e4ac3bf
Andrew Wittrock <andy@wittrock.us>
2025-02-13 17:28:11 -0600

    Only make one update call when updating pagination

--- lib/usePaginator.ts
+++ lib/usePaginator.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Index } from "ts-functional/dist/types";
 
 export declare interface IPaginator {
     current: number;
@@ -8,17 +8,15 @@ export declare interface IPaginator {
     onChange: (page: number, pageSize: number) => void;
 }
 
-export const usePaginator = (pageSize: string, setPageSize:(size:string) => void):IPaginator => {
-    const [current, setCurrent] = useState(1);
-
+export const usePaginator = (current:string, pageSize: string, update:(params:Index<string>) => void):IPaginator => {
     return {
-        current,
+        current: parseInt(current),
         pageSize: parseInt(pageSize),
         defaultPageSize: 12,
         pageSizeOptions: [12, 24, 48, 96],
         onChange: (page: number, pageSize: number) => {
-            setCurrent(page);
-            setPageSize(pageSize.toString());
+            console.log("Page change", page, pageSize);
+            update({page: page.toString(), perPage: pageSize.toString()});
         },
     };
 }
