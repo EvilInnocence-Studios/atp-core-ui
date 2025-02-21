7ab125f27f4a45575bf0fd6f2dca612b812c3a63
Andrew Wittrock <andy@wittrock.us>
2025-02-21 00:00:58 -0600

    Fix TypeScript errors

--- components/YouTube/YouTube.component.tsx
+++ components/YouTube/YouTube.component.tsx
@@ -3,5 +3,5 @@ import styles from './YouTube.module.scss';
 
 export const YouTubeComponent = ({videoId}:YouTubeProps) =>
     <div className={styles.youtube}>
-        <iframe className="youtube-player" type="text/html" src={`//www.youtube.com/embed/${videoId}?wmode=transparent`} allowfullscreen frameborder="0"></iframe>
+        <iframe className="youtube-player" src={`//www.youtube.com/embed/${videoId}?wmode=transparent`} allowFullScreen frameBorder="0"></iframe>
     </div>;
