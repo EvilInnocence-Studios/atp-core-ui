62a9ffa7deee9772e01497328b3c834bfefbb288
Andrew Wittrock <andy@wittrock.us>
2025-03-15 00:06:30 -0500

    Reduce duraction of flash messages

--- lib/flash.ts
+++ lib/flash.ts
@@ -1,8 +1,8 @@
 import { notification } from "antd";
 
 export const flash = {
-    success: (message:string) => () => {notification.success({message})},
-    error:   (message:string) => () => {notification.error(  {message})},
-    warn:    (message:string) => () => {notification.warning({message})},
-    info:    (message:string) => () => {notification.info(   {message})},
+    success: (message:string) => () => {notification.success({message, duration: 2})},
+    error:   (message:string) => () => {notification.error(  {message, duration: 5})},
+    warn:    (message:string) => () => {notification.warning({message, duration: 3})},
+    info:    (message:string) => () => {notification.info(   {message, duration: 2})},
 }
