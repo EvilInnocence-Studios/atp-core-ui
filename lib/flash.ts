1831f3c697fc0902a92cc182c7eeb4efba19798b
Andrew Wittrock <andy@wittrock.us>
2025-03-27 23:59:44 -0500

    Merge flash duraction changes

--- lib/flash.ts
+++ lib/flash.ts
@@ -1,8 +1,8 @@
 import { notification } from "antd";
 
 export const flash = {
-    success: (message:string) => <T>(obj?:T) => {notification.success({message}); return obj;},
-    error:   (message:string) => <T>(obj?:T) => {notification.error(  {message}); return obj;},
-    warn:    (message:string) => <T>(obj?:T) => {notification.warning({message}); return obj;},
-    info:    (message:string) => <T>(obj?:T) => {notification.info(   {message}); return obj;},
+    success: (message:string) => <T>(obj?:T) => {notification.success({message, duration: 2}); return obj;},
+    error:   (message:string) => <T>(obj?:T) => {notification.error(  {message, duration: 5}); return obj;},
+    warn:    (message:string) => <T>(obj?:T) => {notification.warning({message, duration: 3}); return obj;},
+    info:    (message:string) => <T>(obj?:T) => {notification.info(   {message, duration: 2}); return obj;},
 }
