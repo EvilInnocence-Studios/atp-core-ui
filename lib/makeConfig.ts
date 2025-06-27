e53df623c0532089031f81df5c9c62ca0aa2f4c1
Andrew Wittrock <andy@wittrock.us>
2025-06-26 21:18:21 -0500

    Add appType to makeConfig

--- lib/makeConfig.ts
+++ lib/makeConfig.ts
@@ -1,12 +1,12 @@
 import { prop } from "ts-functional";
-import { getMenus, getRoutes, getSettings, IModule } from "./module";
+import { AppType, getMenus, getRoutes, getSettings, IModule } from "./module";
 import { IConfig } from "./types";
 
-export const makeConfig = (apiUrl: string, modules:IModule[]) => ():IConfig => ({
+export const makeConfig = (apiUrl: string, modules:IModule[], appType:AppType) => ():IConfig => ({
     api: {baseUrl: apiUrl},
     modules: modules.map(prop("name")),
-    menus: getMenus(modules, "admin"),
-    routes: getRoutes(modules, "admin"),
+    menus: getMenus(modules, appType),
+    routes: getRoutes(modules, appType),
     settings: getSettings(modules),
     paypal: {
         plans: [],
