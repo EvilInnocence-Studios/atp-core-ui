27863d27abfbd24d09bde6ba6f75b96524792d8f
Andrew Wittrock <andy@wittrock.us>
2025-08-16 13:23:11 -0500

    Remove unneeded import

--- components/AsyncSelect/AsyncSelect.component.tsx
+++ components/AsyncSelect/AsyncSelect.component.tsx
@@ -1,6 +1,5 @@
 import { Select } from "antd";
 import {AsyncSelectProps} from "./AsyncSelect.d";
-import styles from './AsyncSelect.module.scss';
 
 export const AsyncSelectComponent = ({isLoading, getOptions, ...props}:AsyncSelectProps) =>
     <Select {...props} />;
