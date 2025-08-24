e4a6777ba13946a8ef73d8a4cacf94f1ec6cc2ee
Andrew Wittrock <andy@wittrock.us>
2025-08-23 22:15:47 -0500

    add useScript hook

---
+++ lib/useScript.ts
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+export const useScript = (url:string) => {
+    useEffect(() => {
+        if(!url) return;
+        const script = document.createElement('script');
+
+        script.src = url;
+        script.async = true;
+
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, [url]);
+};
