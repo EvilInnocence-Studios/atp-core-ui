75e2779531f0a17d929bb0e7c3c194989be4d731
Andrew Wittrock <andy@wittrock.us>
2025-08-28 21:20:03 -0500

    Add CMS routes

--- components/Content/Content.component.tsx
+++ components/Content/Content.component.tsx
@@ -2,11 +2,14 @@ import { config } from "@config";
 import { NotFoundPage } from "@public/components/NotFoundPage";
 import { Route, Routes } from "react-router";
 import { ContentProps } from "./Content.d";
+import { Page } from "@common/components/Page";
+import { withRoute } from "@core/lib/withRoute";
 
 export const ContentComponent = ({}:ContentProps) =>
     <Routes>
         {config().routes.map((route, index) => (
             <Route key={index} path={route.path} Component={route.component} />
         ))}
+        <Route path="/:slug" Component={withRoute(Page)} />
         <Route path="*" Component={NotFoundPage} />
     </Routes>;
