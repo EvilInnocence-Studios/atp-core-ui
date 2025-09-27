7b477886f8fe695bc61d689f6db4949a8d0f3cce
Andrew Wittrock <andy@wittrock.us>
2025-09-26 22:05:13 -0500

    Remove unneeded import

--- components/S3Image/S3Image.component.tsx
+++ components/S3Image/S3Image.component.tsx
@@ -1,5 +1,4 @@
 import {S3ImageProps} from "./S3Image.d";
-import styles from './S3Image.module.scss';
 
 export const S3ImageComponent = ({fullFileName, className}:S3ImageProps) =>
     <img className={className} src={fullFileName} />;
