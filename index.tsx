99909e053493b130914c1fb48aa52a0e193d8fbc
Andrew Wittrock <andy@wittrock.us>
2026-03-21 21:39:23 -0500

    Add youtube component to layout palette

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import { Content } from "@core/components/Content";
 import { IModule } from "@core/lib/module";
 import { ComponentRegistry } from "@theming/lib/layout/componentRegistry";
 import { coreSettings } from "./lib/settings";
+import { YouTube } from "./components/YouTube";
 
 export const module: IModule = {
     name: "core",
@@ -9,3 +10,4 @@ export const module: IModule = {
 };
 
 ComponentRegistry.register("Content", Content, { category: "Content", displayName: "Page Content" });
+ComponentRegistry.register(YouTube);
